35ec0b264aa12ae99bb04494b5558a32023a1057
Francisco Lucas <fcolucaslima14@gmail.com>
2022-12-30 15:45:13 -0300

    add notification repository

--- src/application/useCases/send-notification.spec.ts
+++ src/application/useCases/send-notification.spec.ts
@@ -1,15 +1,26 @@
+import { Notification } from '../entities/notification';
 import { SendNotification } from './send-notification';
+
+const notifications: Notification[] = [];
+
+const notificationRepository = {
+  create: async (notification: Notification) => {
+    notifications.push(notification);
+  },
+};
 
 describe('Send notification', () => {
   it('should be send a notification', async () => {
-    const sendNotification = new SendNotification();
+    const sendNotification = new SendNotification(notificationRepository);
 
-    const { notification } = await sendNotification.execute({
-      recipientId: 'example-recipient_id',
+    await sendNotification.execute({
+      recipientId: 'example-recipient-id',
       content: 'You have a new message',
       category: 'social',
     });
 
-    expect(notification).toBeTruthy();
+    console.log(notifications);
+
+    expect(notifications).toHaveLength(1);
   });
 });
